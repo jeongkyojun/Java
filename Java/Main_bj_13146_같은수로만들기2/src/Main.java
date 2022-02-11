f1ee7f3faeeab2abe4588fe0f9c4d5dc0b630b40
jeongkyojun <kyojun2002@naver.com>
2022-02-11 16:18:55 +0900

    주석 및 힌트 추가

--- Java/Main_bj_13146_같은수로만들기2/src/Main.java
+++ Java/Main_bj_13146_같은수로만들기2/src/Main.java
@@ -14,10 +14,15 @@ public class Main {
 			Stack<Integer> stk = new Stack<Integer>();
 			int a = Integer.parseInt(bf.readLine());
 			int min = a;
+			/*
+			 * 계단형으로 만들기 -> 가장 작은수부터 시작해 큰수가 나오면 상쇄, 작은수가 나오면 갱신하는대신 스택에 넣는다.
+			 * 큰수가 나오는경우 스택의 값중 현재수보다 큰수가 나올때까지 뺀다.
+			 */
 			for (int i = 1; i < N; i++) {
 				a = Integer.parseInt(bf.readLine());
 				if (min < a) {
 					while (!stk.isEmpty()) {
+						// 수가 크면 스택에서 작은수를 제외한다.
 						int n = stk.pop();
 						if (n > a) {
 							stk.push(n);
@@ -29,6 +34,7 @@ public class Main {
 					min = a;
 				}
 				if (min > a) {
+					//수가 작으면 현재의 수를 스택에 보관하고 갱신한다.
 					stk.push(min);
 					min = a;
 				}
