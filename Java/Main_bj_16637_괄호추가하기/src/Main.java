ad0b82f4226afe0538227314da970888a0e330bb
jeongkyojun <kyojun2002@naver.com>
2022-02-20 12:41:01 +0900

    16637 add

--- Java/Main_bj_16637_괄호추가하기/src/Main.java
+++ Java/Main_bj_16637_괄호추가하기/src/Main.java
@@ -2,63 +2,125 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
 
-	static int Max(int a, int b)
-	{
-		if(a>b)
+	static int max;
+
+	static int Max(int a, int b) {
+		if (a > b)
 			return a;
 		return b;
 	}
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream("res/input_bj_16637.txt")));
-		//BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+		// BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
 		int T = Integer.parseInt(bf.readLine());
-		
-		for(int test_case = 1;test_case <=T;test_case++)
-		{
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			System.out.print("#" + test_case + " ");
+			max = 0;
 			// 줄의 길이
 			int n = Integer.parseInt(bf.readLine());
 			String s = bf.readLine();
-			char[] cal = new char[n/2]; // 9 = 4, 7 = 3
-			int[] num = new int[n/2+1]; // 9 = 5, 7 = 4
-			int[] num2 = new int[n/2]; // num2[i] = num[i] + num[i+1]
-			int p = 0, q = 0;
-			for(int i=0;i<s.length();i++)
-			{
-				if(i%2==0)
-				{
+			char[] cal = new char[n / 2 + 1]; // 계산식을 저장, 마지막은 /로 처리
+			int[] num = new int[n / 2 + 1]; // 숫자를 저장
+			boolean[] B = new boolean[n / 2 + 1];
+			int p = 0;
+			cal[p] = '/';
+			for (int i = 0; i < n; i++) {
+				if (i % 2 == 0) {
 					num[p] = s.charAt(i) - 0x30;
-					if(p>=1)
-					{
-						switch(cal[q-1])
-						{
-						case'+':
-							num2[p-1] = num[p-1]+num[p];
-							break;
-						case'*':
-							num2[p-1] = num[p-1]*num[p];
-							break;
-						case'-':
-							num2[p-1] = num[p-1]-num[p];
-							break;
-						}
+					switch (cal[p]) {
+					case '+':
+						max += num[p];
+						break;
+					case '-':
+						max -= num[p];
+						break;
+					case '*':
+						max *= num[p];
+						break;
+					default:
+						max = num[p];
+						break;
 					}
 					p++;
-				}
-				else
-				{
-					cal[q++] = s.charAt(i); 
-				}
+				} else
+					cal[p] = s.charAt(i);
+
 			}
-			Queue<Integer> Q1 = new LinkedList<Integer>();
-			bw.write("\n");
-			bw.flush();
+			//System.out.println(max);
+			//System.out.println(Arrays.toString(cal));
+			//System.out.println(Arrays.toString(num));
+
+			subset(num, cal, B, num.length - 1, 1);
+			System.out.println(max);
 		}
-		bw.close();
+		bf.close();
 	}
 
+	static void subset(int[] num, char[] cal, boolean[] B, int n, int cnt) {
+		if (n <= cnt) {
+			int res = num[0];
+			for (int i = 1; i < num.length; i++) {
+				if (B[i] && i<num.length-1) {
+					int tmp = num[i];
+					i++;
+					switch (cal[i]) {
+					case '+':
+						tmp += num[i];
+						break;
+					case '-':
+						tmp -= num[i];
+						break;
+					case '*':
+						tmp *= num[i];
+						break;
+					default:
+						break;
+					}
+					switch (cal[i - 1]) {
+					case '+':
+						res += tmp;
+						break;
+					case '-':
+						res -= tmp;
+						break;
+					case '*':
+						res *= tmp;
+						break;
+					default:
+						break;
+					}
+				} else {
+					switch (cal[i]) {
+					case '+':
+						res += num[i];
+						break;
+					case '-':
+						res -= num[i];
+						break;
+					case '*':
+						res *= num[i];
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			if (res > max) {
+				//System.out.print("res : " + res + " , ");
+				//System.out.println(Arrays.toString(B));
+				max = res;
+			}
+			return;
+		}
+		for (int i = cnt; i < num.length; i++) {
+			B[i] = true;
+			subset(num, cal, B, n, cnt + 2);
+			B[i] = false;
+		}
+	}
 }
