4885a84320dce1a809e9274f3aaf4b3773dbac1a
jeongkyojun <kyojun2002@naver.com>
2022-07-09 14:08:29 +0900

    11399 add

--- Java/Main_bj_11399_ATM/src/Main.java
+++ Java/Main_bj_11399_ATM/src/Main.java
@@ -1,9 +1,29 @@
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_11399.txt"));
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(bf.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int b = 0;
+		int res = 0;
+		for(int i=0;i<n;i++)
+		{
+			int num = Integer.parseInt(st.nextToken());
+			pq.offer(num);
+		}
+		for(int i=0;i<n;i++)
+		{
+			int num = pq.poll();
+			res += num+b;
+			b +=num;
+		}
+		System.out.println(res);
 	}
 
 }
