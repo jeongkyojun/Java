d03c6f446e238077bd1b04d5a474e881d4602d9d
jeongkyojun <kyojun2002@naver.com>
2023-02-11 12:45:37 +0900

    10866 add

--- Main_bj_10866_덱/src/Main.java
+++ Main_bj_10866_덱/src/Main.java
@@ -20,33 +20,60 @@ public class Main {
 			{
 				StringTokenizer st = new StringTokenizer(bf.readLine());
 				String str = st.nextToken();
+//				System.out.print(str+" ");
 				if(str.equals("push_back")) {
 					int value = Integer.parseInt(st.nextToken());
 					deck[back++] = value;
 					back%=size;
+//					System.out.println();
 				}else if(str.equals("push_front")) {
 					int value = Integer.parseInt(st.nextToken());
 					head--;
 					if(head<0) head+=size;
 					deck[head] = value;
+//					System.out.println();
 				}else if(str.equals("pop_back")) {
-					back--;
-					if(back<0) back+=size;
-					System.out.println(deck[back]);
+					if(isEmpty(head,back))
+					{
+						System.out.println("-1");
+					}else {	
+						back--;
+						if(back<0) back+=size;
+						System.out.println(deck[back]);
+					}
 				}else if(str.equals("pop_front")) {
-					System.out.println(deck[head++]);
-					head%=size;
+					if(isEmpty(head,back))
+					{
+						System.out.println("-1");
+					}else {	
+						System.out.println(deck[head++]);
+						head%=size;
+					}
 				}else if(str.equals("front")) {
-					
+					if(isEmpty(head,back))
+					{
+						System.out.println("-1");
+					}else {	
+						System.out.println(deck[head]);
+					}
 				}else if(str.equals("back")) {
-					
+					if(isEmpty(head,back))
+					{
+						System.out.println("-1");
+					}else {	
+						System.out.println(deck[(back-1+size)%size]);
+					}
 				}else if(str.equals("size")) {
-					
+					System.out.println((back-head+size)%size);
 				}else if(str.equals("empty")) {
-					
+					System.out.println(isEmpty(head,back)?"1":"0");
 				}
 			}
 		}
 	}
-
+	
+	static boolean isEmpty(int head, int tail) {
+		if(head==tail) return true;
+		return false;
+	}
 }
